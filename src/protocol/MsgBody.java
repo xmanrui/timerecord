e120b53384f8ac300cbc4014877427d00dabcb17
xiemanrui@foxmail.com <xiemanrui@foxmail.com>
2017-01-01 16:27:13 +0800

    完善消息体类

--- src/protocol/MsgBody.java
+++ src/protocol/MsgBody.java
@@ -1,9 +1,8 @@
 package protocol;
 
 import java.util.ArrayList;
-import static protocol.Tool.AddLowByteToArrList;
-import static protocol.Tool.AddHighAndLowByteToArrList;
-import static protocol.Tool.PackageByteList;
+import static protocol.Tool.*;
+import static protocol.Tool.StrChangeToBytes;
 
 public abstract class MsgBody {
     public int msgID = 0;
@@ -98,3 +97,73 @@ class ReSendSubpackageRequestMsgBody extends MsgBody
         }
     }
 }
+
+class ClientRegisterMsgBody extends MsgBody
+{
+    public int provinceID = 0;
+    public int cityID = 0;
+    public int[] manufacturerID = new int[5];
+    public int[] clientType = new int[20];
+    public int[] serialNumber = new int[7];
+    public int[] IMEI = new int[15];
+    public int carColor = 0;
+    public String carID = "";
+
+    public ClientRegisterMsgBody()
+    {
+        super(0x0100);
+    }
+
+    public void MakeMsgBodyByteList()
+    {
+        msgBodyBytesList.clear();
+        AddLowByteToArrList(provinceID, msgBodyBytesList);
+        AddLowByteToArrList(cityID, msgBodyBytesList);
+        AddLowBytesToArrList(manufacturerID, msgBodyBytesList);
+        AddLowBytesToArrList(clientType, msgBodyBytesList);
+        AddLowBytesToArrList(serialNumber, msgBodyBytesList);
+        AddLowBytesToArrList(IMEI, msgBodyBytesList);
+        AddLowByteToArrList(carColor, msgBodyBytesList);
+        AddBytesToArrList(StrChangeToBytes(carID), msgBodyBytesList);
+    }
+}
+
+class ClientRegisterAckMsgBody extends MsgBody
+{
+    int ackID = 0;
+    int result = 0;
+    int[] platformID = new int[5];
+    int[] trainingInstitutionsID =  new int[16];
+    int[] timeClientID = new int[16];
+    String clientCertificate = "";
+
+    public ClientRegisterAckMsgBody()
+    {
+        super(0x8100);
+    }
+
+    public void MakeMsgBodyByteList()
+    {
+        msgBodyBytesList.clear();
+        AddLowByteToArrList(ackID, msgBodyBytesList);
+        AddLowByteToArrList(result, msgBodyBytesList);
+        AddLowBytesToArrList(platformID, msgBodyBytesList);
+        AddLowBytesToArrList(trainingInstitutionsID, msgBodyBytesList);
+        AddLowBytesToArrList(timeClientID, msgBodyBytesList);
+        AddBytesToArrList(StrChangeToBytes(clientCertificate), msgBodyBytesList);
+    }
+}
+
+class ClientLogoutMsgBody extends MsgBody
+{
+    public ClientLogoutMsgBody()
+    {
+        super(0x0003);
+    }
+
+    public void MakeMsgBodyByteList()
+    {
+        msgBodyBytesList.clear();
+    }
+}
+
