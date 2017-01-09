da3cb0c28b5c9f96d3de5e5474717b63f0b02008
xiemanrui@foxmail.com <xiemanrui@foxmail.com>
2017-01-10 00:26:03 +0800

    增加一个校验函数

--- src/protocol/CheckCode.java
+++ src/protocol/CheckCode.java
@@ -6,15 +6,32 @@ public final class CheckCode {
     {
         byte temp = msgHeader[0];
 
-        for(int i=1; i < msgHeader.length-1; i++)
+        for(int i=1; i < msgHeader.length; i++)
         {
             temp ^= msgHeader[i];
         }
 
-        for(int i=0; i<msgBody.length-1; i++)
+        for(int i=0; i<msgBody.length; i++)
         {
             temp ^= msgBody[i];
         }
+        return temp;
+    }
+
+    public static byte MakeCheckedCode(byte[] arr)
+    {
+        if (arr.length == 0)
+        {
+            return (byte)0;
+        }
+
+        byte temp = arr[0];
+
+        for(int i=1; i<arr.length; i++)
+        {
+            temp ^= arr[i];
+        }
+
         return temp;
     }
 }
